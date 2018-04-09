a054bed4d976b3f7484a04719206b4ef4b759f0f
helder <helder.ferreira@pingpost.pt>
2018-04-09 14:59:59 +0100

    -> Added css loader, file loader and an image loader to the project

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,35 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            // Specify the path where the images will be saved
+                            outputPath: './images'
+                        }
+                    },
+                    {
+                        // Image loader
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                        },
+                    }
+                ]
+            }
+        ]
     }
 };
