1694fde9b277891c363ba084fd27dcb963995890
helder <helder.ferreira@pingpost.pt>
2018-04-09 18:22:37 +0100

    -> Create a single output bundle based on 2 different .js entries

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@
  * resolve this. If we need to use some new features from es2015/ES6, we must use another transpiler like babel to support
  * read this https://webpack.js.org/guides/getting-started/#modules to learn how to do it*/
 import _ from 'lodash';
+import printMe from './print.js';
 
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
