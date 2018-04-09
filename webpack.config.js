9b13d08bf52ca13ac495bc30a39783b0e14dd15d
helder <helder.ferreira@pingpost.pt>
2018-04-09 18:27:32 +0100

    -> Inactive the base bundle from the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        // app: './src/index.js',
-        // print: './src/print.js'
-        base: ['./src/index.js', './src/print.js']
+        app: './src/index.js',
+        print: './src/print.js'
+        // base: ['./src/index.js', './src/print.js'] Create a new bundle gathering these 2 scripts
     },
     output: {
         filename: '[name].bundle.js',
