9bb528f4b95d89ec87a7dedab0f863529e4f7953
helder <helder.ferreira@pingpost.pt>
2018-04-09 15:44:01 +0100

    -> Remove some assets that won't be needed for this branch

--- src/index.js
+++ src/index.js
@@ -2,41 +2,16 @@
  * resolve this. If we need to use some new features from es2015/ES6, we must use another transpiler like babel to support
  * read this https://webpack.js.org/guides/getting-started/#modules to learn how to do it*/
 import _ from 'lodash';
-// Webpack has loaders in his ecosystem that permits to load certain styles to our js modules/components :D AMAZING!
-import './style.css';
-import IconWindowsUrl from './images/activedirectory.png';
-import jsonData from './jsonData/MOCK_DATA.json';
+
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    let myIcon = new Image();
-    myIcon.src = IconWindowsUrl;
-    element.appendChild(myIcon);
 
     return element;
 }
 
-function loadData() {
-    const data = jsonData;
-    const Person = {
-        fullName: function () {
-            return `Name: ${this.name} ${this.email}`;
-        }
-    };
-
-    _.forEach(data, (person) => {
-        let currentPerson = Object.create(Person);
-        currentPerson.name = person.first_name;
-        console.log(currentPerson.fullName());
-    });
-}
-
 document.body.appendChild(component());
 
-loadData();
--- webpack.config.js
+++ webpack.config.js
@@ -5,42 +5,5 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            // Specify the path where the images will be saved
-                            outputPath: './images'
-                        }
-                    },
-                    {
-                        // Image loader to optimize the images
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassOnDebug: true,
-                        },
-                    }
-                ]
-            },
-            // User file loader to handle font files
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
-        ]
     }
 };
