385c65dfe1226090642cb58f4bf511f99d9d4154
helder <helder.ferreira@pingpost.pt>
2018-04-10 17:23:49 +0100

    -> Added clean webpack plugin to the project, to clean the dist folder files

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
         app: './src/index.js',
         print2: './src/print.js'
-        // base: ['./src/index.js', './src/print.js'] //Create a new bundle gathering these 2 scripts
+        //base: ['./src/index.js', './src/print.js'] //Create a new bundle gathering these 2 scripts
     },
     plugins: [
         // Este plugin permite que ao alterarmos as nossas entries,
         // Que crie automaticamente um novo html com as novas dependencias, eliminando as antigas, sem termos que fazer isto
         // manualmente (O problema aqui e como vou integrar isto com os templates de twig html do room presence)
+        // A integraçao c/ o symphony nao pode ser feita desta forma, temos que manualmente alterar os caminhos caso as entries sejam alteradas
         // new HtmlWebpackPlugin({
         //     template: 'src/index.html.twig',
         //     filename: 'index.html.twig'
         // })
         new HtmlWebpackPlugin({
-            title: 'Output Management'
-        })
+            title: 'Output Management',
+            template: 'src/index.html'
+        }),
+        // Permite limpar o conteudo do directorio (limpa ficheiros que nao estao a ser usados)
+        new CleanWebpackPlugin(['dist'])
     ],
     output: {
         filename: '[name].bundle.js',
